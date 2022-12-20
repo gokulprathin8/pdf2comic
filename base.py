c62060202c51bdbbc3fd55c0cf31863fe9c2c644
Gokul Prathin <gokulprathin8@gmail.com>
2022-12-20 16:00:07 -0500

    completed base idea

---
+++ base.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import uuid
+import shutil
+from copy import deepcopy
+from pdf2image import convert_from_path
+
+cmd_args = sys.argv
+file_name = 'sample.pdf'
+
+file_lst = deepcopy(cmd_args)
+file_lst = file_lst[1:]
+
+current_wk_dir = os.getcwd()
+target_file = current_wk_dir + file_name
+rand_hex = str(uuid.uuid4().hex)
+
+for file in file_lst:
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+    pdf_images = convert_from_path(file_name)
+    for page_order in range(len(pdf_images)):
+        pdf_images[page_order].save('tmp/' + 'page' + str(page_order) + rand_hex + '.jpg', 'JPEG')
+    file_no_ext = file[:-4]
+    shutil.make_archive(f'{file_no_ext}', 'zip', 'tmp')
+
+    os.rename(f'{file_no_ext}.zip', f'{file_no_ext}.cbz')
+    shutil.rmtree('tmp')
+
