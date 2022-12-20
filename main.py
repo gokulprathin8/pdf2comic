f2bd2cf3c3238450d858b6ba7e0507d6a7939d6d
Gokul Prathin <gokulprathin8@gmail.com>
2022-12-20 15:44:27 -0500

    added requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 from pdf2image import convert_from_path
 
 # file_name = sys.argv[1]
@@ -8,6 +9,13 @@ file_name = 'sample.pdf'
 current_wk_dir = os.getcwd()
 target_file = current_wk_dir + file_name
 
+if not os.path.exists('tmp'):
+    os.makedirs('tmp')
+
 pdf_images = convert_from_path(file_name)
 for i in range(len(pdf_images)):
-    pdf_images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+    pdf_images[i].save('tmp/' + 'page' + str(i) + '.jpg', 'JPEG')
+
+shutil.make_archive('output', 'zip', 'tmp')
+
+os.rename('output.zip', 'output.cbz')
